dd64f1f69444580d542ce6baad56724f40053157
KONY128 <lhy159840@outlook.com>
2020-07-09 14:54:56 +0800

    build: add teacher navBar component

---
+++ src/views/teacher/TeacherMain.vue
@@ -0,0 +1,12 @@
+<template>
+    <NaviBarStudent></NaviBarStudent>
+</template>
+
+<script>
+    import NaviBarStudent from './components/NaviBarStudent'
+    export default {
+        components: {
+            NaviBarStudent,
+        }
+    }
+</script>
