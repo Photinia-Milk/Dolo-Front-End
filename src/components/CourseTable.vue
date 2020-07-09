166e94f9b08f20c28c8603f5bed1e69d79cd647b
Jinke Liu <lytgyqljk@163.com>
2020-07-09 17:16:32 +0800

    chore: create the courseTable

--- src/components/CourseTable.vue
+++ src/components/CourseTable.vue
@@ -1,57 +1,51 @@
 <template>
-    <div class="tableContainer">
-    <div class="tableRoot">
-        <el-table
-                :data="tableData"
-                style="width: 100%">
-            <el-table-column
-                    prop="date"
-                    label="日期"
-                    width="180">
-            </el-table-column>
-            <el-table-column
-                    prop="name"
-                    label="姓名"
-                    width="180">
-            </el-table-column>
-            <el-table-column
-                    prop="address"
-                    label="地址">
-            </el-table-column>
-        </el-table>
-    </div>
-    </div>
+    <vue-tuicalendar
+            ref="calendar"
+            :options="options"
+            :schedules="schedules"
+            @after-render-schedule="handler"
+            @before-render-schedule="handler"
+            @click-schedule="handler"
+    >
+    </vue-tuicalendar>
 </template>
+
 
 <script>
     export default {
+        name: "CourseTable",
         data() {
             return {
-                tableData: [{
-                    date: '2016-05-02',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1518 弄'
-                }, {
-                    date: '2016-05-04',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1517 弄'
-                }, {
-                    date: '2016-05-01',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1519 弄'
-                }, {
-                    date: '2016-05-03',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1516 弄'
-                }]
+                schedules: [
+                    {
+                        id: "1",
+                        calendarId: "1",
+                        title: "A schedule",
+                        category: "time",
+                        dueDateClass: "",
+                        start: "2018-05-22T22:30:00+09:00",
+                        end: "2018-05-23T02:30:00+09:00"
+                    },
+                    {
+                        id: "2",
+                        calendarId: "1",
+                        title: "Another schedule",
+                        category: "time",
+                        dueDateClass: "",
+                        start: "2018-05-23T17:30:00+09:00",
+                        end: "2018-05-24T17:31:00+09:00",
+                        isReadOnly: true
+                    }
+                ]
+            }
+        },
+
+        methods: {
+            mounted() {
+                this.$refs.calendar.fireMethod('clear');
+                this.$refs.calendar.fireMethod('getElement');
+                this.$refs.calendar.fireMethod('changeView', 'month', true);
             }
         }
     }
 </script>
-
-<style scoped>
-.tableContainer{
-    margin-left: 300px;
-    width: 800px
-}
-</style>
