60436592ff98880fa3bf5ae86d619d6cc2def030
Jinke Liu <lytgyqljk@163.com>
2020-07-09 13:53:57 +0800

    build: create a login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,16 +3,18 @@
         <el-card class="boxCard" shadow="hover" body-style="{ padding: '0px' }">
             <img class="img" src="@/assets/Login/login.jpg" alt="login">
             <div style="display:flex;justify-content: center;align-items:center; margin-top: 1px">
-                <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm" style="margin-top:20px;">
-                    <el-form-item label="手机号" prop="phone" >
-                        <el-input v-model.number="ruleForm.phone" class="input" style="margin-right:200px"></el-input>
+                <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+                    <el-form-item label="账户" prop="pass">
+                        <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
                     </el-form-item>
-                    <el-form-item label="密码" prop="pass">
-                        <el-input type="password" v-model="ruleForm.pass" autocomplete="off"  class="input"></el-input>
+                    <el-form-item label="密码" prop="checkPass">
+                        <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
+                    </el-form-item>
+                    <el-form-item label="确认密码" prop="age">
+                        <el-input v-model.number="ruleForm.age"></el-input>
                     </el-form-item>
                     <el-form-item>
-                        <el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>
-                        <el-button type="success" @click="registry()">注册</el-button>
+                        <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
                         <el-button @click="resetForm('ruleForm')">重置</el-button>
                     </el-form-item>
                 </el-form>
