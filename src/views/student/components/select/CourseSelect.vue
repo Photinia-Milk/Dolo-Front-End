3cbaa020cecc1f984b83b625c52a03a1823ca61e
KONY128 <lhy159840@outlook.com>
2020-07-10 13:59:44 +0800

    feature: add school articles component

--- src/views/student/components/select/CourseSelect.vue
+++ src/views/student/components/select/CourseSelect.vue
@@ -2,7 +2,10 @@
     <el-container class="container">
         <div class="select-container">
             <div class="select-left-container">
-                <SearchBox class="search"></SearchBox>
+                <SearchBox style="margin-bottom: 40px;"></SearchBox>
+                <div v-for="item in articles" :key="item.title">
+                    <SchoolArticles :title="item.title" :content="item.content"></SchoolArticles>
+                </div>
             </div>
             <div class="select-right-container">
                 <CourseTable class="table" width="500"></CourseTable>
@@ -14,17 +17,34 @@
 <script>
     import SearchBox from "./components/SearchBox";
     import CourseTable from "./components/CourseTable";
+    import SchoolArticles from "./components/SchoolArticles";
     export default {
         name: "CourseSelect",
+        data(){
+            return{
+                "articles": [
+                    {
+                        "title": "本学期选课已正式开始",
+                        "content": "请同学们及时选择本学期课程，\n避免错过正常的选课时间\n\n本学期的选课时段为：\n2018-07-01 - 2018-08-14\n 逾期选课系统即关闭",
+                    },
+                    {
+                        "title": "这是一篇测试",
+                        "content": "你们做的非常好！\n请继续努力",
+                    }
+                ],
+            }
+        },
         components: {
             SearchBox,
-            CourseTable
+            CourseTable,
+            SchoolArticles
         }
     }
 </script>
 
 <style scoped>
     .select-left-container{
+        width: auto;
         display: flex;
         flex-direction: column;
     }
--- src/views/student/components/select/components/SchoolArticles.vue
+++ src/views/student/components/select/components/SchoolArticles.vue
@@ -1,80 +1,29 @@
 <template>
-    <el-card id="courseTable" class="box-card" style="width: 430px;">
-        <el-table
-                :data="tableData"
-                style="width: 100%">
-            <el-table-column
-                    fixed
-                    label="课程名称"
-                    width="260">
-                <template slot-scope="scope">
-                    <el-popover trigger="hover" placement="top">
-                        <p>教学班: {{ scope.row.class }}</p>
-                        <p>上课教师: {{ scope.row.teacher }}</p>
-                        <div slot="reference" class="name-wrapper">
-                            <el-tag size="medium">{{ scope.row.name }}</el-tag>
-                        </div>
-                    </el-popover>
-                </template>
-            </el-table-column>
-            <el-table-column
-                    label="教室"
-                    width="95">
-                <template slot-scope="scope">
-                    <el-popover trigger="hover" placement="top">
-                        <p>校区: {{ scope.row.area }}</p>
-                        <p>课程备注: {{ scope.row.remarks}}</p>
-                        <div slot="reference" class="name-wrapper">
-                            <el-tag size="medium">{{ scope.row.location }}</el-tag>
-                        </div>
-                    </el-popover>
-                </template>
-            </el-table-column>
-            <el-table-column
-                    label="种类"
-                    width="75">
-                <template slot-scope="scope">
-                    <!--                <i class="el-icon-time"></i>-->
-                    <span style="margin-left: 10px">{{ scope.row.type }}</span>
-                </template>
-            </el-table-column>
-            <el-table-column
-                    fixed="right"
-                    label="状态"
-                    width="140">
-                <template slot-scope="scope">
-                    <div  v-if="scope.row.if_select===false">
-                        <span style="margin-left: 10px">{{ scope.row.current }}/{{scope.row.max}}</span>
-                    </div>
-                    <div v-else>
-                        <span style="margin-left: 10px">{{ scope.row.current + 1}}/{{scope.row.max}}</span>
-                    </div>
-                </template>
-            </el-table-column>
-            <el-table-column
-                    fixed="right"
-                    label="操作"
-            >
-                <template slot-scope="scope">
-                    <div  v-if="scope.row.if_select === true">
-                        <el-button
-                                size="mini"
-                                type="danger"
-                                @click="handleDrop(scope.$index, scope.row)">退选</el-button>
-                    </div>
-                    <div v-else>
-                        <el-button
-                                size="mini"
-                                @click="handleSelect(scope.$index, scope.row)">选课</el-button>
-                    </div>
-                </template>
-            </el-table-column>
-        </el-table>
+    <el-card id="courseTable" class="box-card" style="width: 620px; margin-bottom:10px;">
+        <div slot="header" class="clearfix">
+            <span class="card-header">{{cardTitle}}</span>
+        </div>
+        <div class="text-wrapper">
+            {{cardContent}}
+        </div>
     </el-card>
 
 </template>
 
 <style>
+    .text-wrapper{
+        line-height: 150%;
+        white-space: pre-wrap;
+        color: #A09691;
+    }
+    .card-header{
+        font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
+        color: #808080;
+        font-weight: bold;
+    }
+    .box-card {
+        width: 480px;
+    }
     .text {
         font-size: 14px;
     }
@@ -98,116 +47,24 @@
 </style>
 <script>
     export default {
+        props:{
+            'title':String,
+            'content':String,
+        },
         data() {
             return {
-                tableData: [{
-                    class: '(2019-2020-1)-SE118-1',
-                    name: '计算机系统基础（汇编）',
-                    teacher: '臧斌宇,陈榕',
-                    area: '闵行',
-                    location: '中院413',
-                    score: 3,
-                    time: '星期一第3-4节{1-16周}\n' +
-                        '星期三第1-2节{1-15周(单)}',
-                    remarks: '实践环节：虚拟时间',
-                    type: '必修',
-                    max: 121,
-                    current: 100,
-                    if_select: false
-                }, {
-                    class: '(2019-2020-1)-SE118-1',
-                    name: '计算机系统基础（汇编）',
-                    teacher: '臧斌宇,陈榕',
-                    area: '闵行',
-                    location: '中院413',
-                    score: 3,
-                    time: '星期一第3-4节{1-16周}\n' +
-                        '星期三第1-2节{1-15周(单)}',
-                    remarks: '实践环节：虚拟时间',
-                    type: '必修',
-                    max: 121,
-                    current: 100,
-                    if_select: false
-                }, {
-                    class: '(2019-2020-1)-SE118-1',
-                    name: '计算机系统基础（汇编）',
-                    teacher: '臧斌宇,陈榕',
-                    area: '闵行',
-                    location: '中院413',
-                    score: 3,
-                    time: '星期一第3-4节{1-16周}\n' +
-                        '星期三第1-2节{1-15周(单)}',
-                    remarks: '实践环节：虚拟时间',
-                    type: '必修',
-                    max: 121,
-                    current: 100,
-                    if_select: false
-                }, {
-                    class: '(2019-2020-1)-SE118-1',
-                    name: '计算机系统基础（汇编）',
-                    teacher: '臧斌宇,陈榕',
-                    area: '闵行',
-                    location: '中院413',
-                    score: 3,
-                    time: '星期一第3-4节{1-16周}\n' +
-                        '星期三第1-2节{1-15周(单)}',
-                    remarks: '实践环节：虚拟时间',
-                    type: '必修',
-                    max: 121,
-                    current: 100,
-                    if_select: true
-                }, {
-                    class: '(2019-2020-1)-SE118-1',
-                    name: '计算机系统基础（汇编）',
-                    teacher: '臧斌宇,陈榕',
-                    area: '闵行',
-                    location: '中院413',
-                    score: 3,
-                    time: '星期一第3-4节{1-16周}\n' +
-                        '星期三第1-2节{1-15周(单)}',
-                    remarks: '实践环节：虚拟时间',
-                    type: '必修',
-                    max: 121,
-                    current: 100,
-                    if_select: true
-                }
-                ]
+                'cardTitle': this.title,
+                'cardContent': this.content,
             }
         },
+        created() {
+        },
         methods: {
-            handleSelect(index, row) {
-                console.log("before");
-                console.log(index, row);
-                if(row.if_select === false){
-                    row.if_select = true;
-                }
-                this.$message({
-                    type: 'success',
-                    message: '选课成功!'
-                });
-                console.log("after");
-                console.log(index, row);
-            },
-            handleDrop(index, row) {
-                this.$confirm('是否退选此门课程？', '提示', {
-                    confirmButtonText: '确定',
-                    cancelButtonText: '取消',
-                    type: 'warning'
-                }).then(() => {
-                    if(row.if_select === true){
-                        row.if_select = false;
-                    }
-                    console.log(index, row);
-                    this.$message({
-                        type: 'success',
-                        message: '退选成功!'
-                    });
-                }).catch(() => {
-                    this.$message({
-                        type: 'info',
-                        message: '已取消退选'
-                    });
-                });
+            getParent(){
+                this.cardTitle = this.home.title;
+                console.log(this.home.title);
+                console.log(this.cardTitle);
+                this.cardContent = this.home.content;
             }
         },
         name: "CourseTable"
