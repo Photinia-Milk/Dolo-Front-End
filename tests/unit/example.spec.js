8ebadd7b4e3b12fca6a241bc1f21886fce66c944
maohaotian <mao8816229>
2020-07-16 17:10:58 +0800

    feat: for test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,1 +1,10 @@
 import {shallowMount,createLocalVue,mount} from "@vue/test-utils";
+import Login2 from "../../src/views/login/Login";
+import ElementUI from "element-ui"
+
+const localVue=createLocalVue();
+localVue.use(ElementUI);
+
+
+const login=Login2;
+
