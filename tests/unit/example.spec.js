c886edf0895db2e41a5a79362c64ee314622175b
Jinke Liu <lytgyqljk@163.com>
2020-07-16 17:23:40 +0800

    fix: Fixed json configuration

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,11 +1,11 @@
 // eslint-disable-next-line no-unused-vars
-import {shallowMount,createLocalVue,mount} from "@vue/test-utils";
-import Login2 from "../../src/views/login/Login";
-import ElementUI from "element-ui"
-
-const localVue=createLocalVue();
-localVue.use(ElementUI);
-
-
-const login=Login2;
+// import {shallowMount,createLocalVue,mount} from "@vue/test-utils";
+// import Login2 from "../../src/views/login/Login";
+// import ElementUI from "element-ui"
+//
+// const localVue=createLocalVue();
+// localVue.use(ElementUI);
+//
+//
+// const login=Login2;
 
